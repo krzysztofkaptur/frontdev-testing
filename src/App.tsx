c9006ce3700c10a45c699529775c7d0c94192f96
krzysztofkaptur <krzysztof.kaptur@gmail.com>
2026-02-07 12:49:57 +0100

    init

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import { Routes, Route, Link } from "react-router";
+import './App.css'
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function App() {
+  return (
+    <>
+      <nav>
+        <Link to="/home">Home</Link>{" | "}
+        <Link to="/about">About</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </>
+  )
+}
+
+export default App
