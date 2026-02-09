58289fa05a69bbd163c8a697bc18db3a10491952
krzysztofkaptur <krzysztof.kaptur@gmail.com>
2026-02-09 18:42:00 +0100

    fix formatting/linting pipeline

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,12 +1,10 @@
-import { describe, expect, it } from "vitest";
 import { render, screen } from "@testing-library/react";
 import { MemoryRouter } from "react-router";
+import { describe, expect, it } from "vitest";
 import App from "./App";
 
 describe("App", () => {
 	it("should render the App title", () => {
-		const test = {}
-
 		render(
 			<MemoryRouter>
 				<App />
